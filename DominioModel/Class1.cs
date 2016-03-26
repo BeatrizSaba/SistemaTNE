612f33adad42fd586dd6b9f5da9962ab3e0c3010
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2016-03-26 17:18:24 -0300

    Criação/Modificação na estrutura do projeto.

---
+++ DominioModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioModel
+{
+    public class Class1
+    {
+    }
+}
