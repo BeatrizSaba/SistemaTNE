29e5ef175e7937bf3b0147b210bd02a54399deee
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2016-03-26 21:52:48 -0300

    Adição da classe cliente ao git

---
+++ DominioModel/Entidades/Cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioModel.Entidades
+{
+    public class Cliente
+    {
+
+        public int ClienteID { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime DataModificacao { get; set; }
+
+        public string Nome { get; set; }
+        
+        public DateTime DataNascimento { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+
+        public List<Posto> PostoFavoritos { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public List<FormaPagamento> FormasPagamento { get; set; }
+
+        public List<Servico> ServicosUtilizados { get; set; }
+
+        public FrequenciaCompra FrequenciaCompra { get; set; }
+
+        public EstadoCliente Estado { get; set; }
+
+        public RamoAtividade RamoAtividade { get; set; }
+
+        public TipoPessoa Tipo { get; set; }
+
+        public Marca MarcaPreferida { get; set; }
+    }
+}
